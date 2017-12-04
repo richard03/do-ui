f2a5f2f8485cfd4bbda6ad1a03e6d21f96940640
richard@prokoho.cz <richard@prokoho.cz>
2017-12-04 04:07:43 +0100

    build related files for windows+git+gulp+webpack

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+ var gulp = require('gulp');
+
+
+gulp.task('default', function () {
+  var src = [
+		'./src/*.html',
+		'./src/css/*.css'
+	];
+
+	return gulp.src(src)
+		.pipe(gulp.dest('./build'));
+});
+
+// gulp.task('watch', function () {
+// 	gulp.watch('./src/*.html', ['default']);
+// 	gulp.watch('./src/css/*.css', ['default']);
+// });
