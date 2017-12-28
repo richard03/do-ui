481adb8cbc626bb5102234e18fc608475676c924
richard@prokoho.cz <richard@prokoho.cz>
2017-12-28 17:26:55 +0100

    TaskListReducer - tasks with status "done" no longer appear in the taskList

--- src/jsx/reducers/TaskListReducer.jsx
+++ src/jsx/reducers/TaskListReducer.jsx
@@ -24,7 +24,18 @@ export default {
 								login: context.store.getState().loginReducer.login
 							})
 						.then(
-							tasks => context.store.dispatch({ type: 'populateTaskList', tasks: tasks }),
+							// on success
+							function (allTasks) {
+								// do some filtering, output to associative array
+								let tasks = {}
+								allTasks.forEach( function (task) {
+									if (task.status != 'done') {
+										tasks[task.id] = Object.assign({}, task)
+									}
+								})
+								context.store.dispatch({ type: 'populateTaskList', tasks })
+							},
+							// on failure
 							() => context.store.dispatch({ type: 'reportFailure' })
 						)
 					return Object.assign({}, state, { taskListLoading: true });
